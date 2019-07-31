4f8f17d96bd99c59598c7a18916d167321cf1628
Phil Nash <philnash@gmail.com>
2019-07-31 10:50:03 +1000

    Removes list-templates implementation in favour of twilio-run

--- src/cli.js
+++ src/cli.js
@@ -1,22 +1,16 @@
 const yargs = require('yargs');
-const listTemplates = require('./commands/list-templates');
 const DefaultCommand = require('./command');
+const ListTemplateCommand = require('twilio-run/dist/commands/list-templates');
 
 function cli(cwd) {
   yargs.help();
   yargs.alias('h', 'help');
   yargs.version();
   yargs.alias('v', 'version');
-
   yargs.default('path', cwd);
   yargs.usage(DefaultCommand.describe);
   yargs.command(DefaultCommand);
-  yargs.command({
-    command: 'list-templates',
-    desc: 'List the available templates you can create a project with.',
-    handler: argv => listTemplates(argv)
-  });
-
+  yargs.command(ListTemplateCommand);
   return yargs;
 }
 

5becf61fd8745bb0085cea0ccbaf618fca3710fb
Dirk Lemstra <dirk@lemstra.org>
2019-10-12 11:51:01 +0200

    Switch to configuration from the twilio-style project.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,11 @@
 module.exports = {
-  'extends': 'standard',
+  'extends': 'twilio',
   'plugins': ['jest'],
-  'rules': {
-    'indent': ['error', 2, { 'SwitchCase': 1 }],
-    'semi': ['error', 'always'],
-    'space-before-function-paren': ['error', {
-      'anonymous': 'never',
-      'named': 'never',
-      'asyncArrow': 'always'
-    }],
-  },
   'env': {
     'jest/globals': true
+  },
+  'parserOptions': {
+    'ecmaVersion': 9,
+    'sourceType': 'module',
   }
 }

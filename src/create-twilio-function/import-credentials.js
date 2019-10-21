08ff4a810a5baf19d94f8030a8038ea137282ae1
Dirk Lemstra <dirk@lemstra.org>
2019-10-21 23:46:33 +0200

    Revert incorrect patch.

--- src/create-twilio-function/import-credentials.js
+++ src/create-twilio-function/import-credentials.js
@@ -30,8 +30,8 @@ async function importCredentials(config) {
     return credentials;
   }
 
-  const { result } = await inquirer.prompt(questions);
-  return result ? credentials : {};
+  const { importCredentials } = await inquirer.prompt(questions);
+  return importCredentials ? credentials : {};
 }
 
 module.exports = importCredentials;

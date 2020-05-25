ab02b5a82d27826bbaf677065df48ab8c4579523
Phil Nash <philnash@gmail.com>
2020-05-25 11:13:03 +1000

    Adds some types to example TS function.

--- templates/typescript/src/functions/hello-world.ts
+++ templates/typescript/src/functions/hello-world.ts
@@ -7,12 +7,23 @@ import {
   ServerlessFunctionSignature,
 } from '@twilio-labs/serverless-runtime-types/types';
 
+type MyEvent = {
+  Body?: string
+}
+
+// If you want to use environment variables, you will need to type them like
+// this and add them to the Context in the function signature as
+// Context<MyContext> as you see below.
+type MyContext = {
+  GREETING?: string
+}
+
 export const handler: ServerlessFunctionSignature = function(
-  context: Context,
-  event: {},
+  context: Context<MyContext>,
+  event: MyEvent,
   callback: ServerlessCallback
 ) {
   const twiml = new Twilio.twiml.VoiceResponse();
-  twiml.say('Hello World!');
+  twiml.say(`${context.GREETING ? context.GREETING : 'Hello'} ${event.Body ? event.Body : 'World'}!`);
   callback(null, twiml);
 };
